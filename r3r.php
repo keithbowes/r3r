c66e770b7bdc00f4761947a4f8fb877c8d3a902c
zooplah <>
2005-06-17 21:43:14 +0000

    Committed more files for 1.2.x (hopefully the last of 'em); Updated README to the trunk version

---
+++ r3r.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('includes/l10n.php');
+
+if (!extension_loaded('php-gtk'))
+  if (!dl('php_gtk.' . PHP_SHLIB_SUFFIX))
+    trigger_error(ALERT_NO_PHP_GTK, E_USER_ERROR);
+
+require_once('version.php');
+
+require_once('includes/globals.php');
+require_once('includes/mainAppGui.php');
+require_once('includes/settings.php');
+
+startMainGui();
+
+?>
