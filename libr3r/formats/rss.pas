82462b073c95674a30716b635051576c9eac685a
zooplah <>
2005-06-28 15:48:40 +0000

    Finished filling in the RSS 2.0 Item

--- libr3r/formats/rss.pas
+++ libr3r/formats/rss.pas
@@ -39,9 +39,47 @@ begin
     begin
       Item.Title := Item.Title + Content;
     end
+    else if Name = 'description' then
+    begin
+      Item.Description := Item.Description + Content;
+    end
     else if Name = 'link' then
     begin
       Item.Links.Add(Content);
+    end
+    else if Name = 'category' then
+    begin
+      Item.Subject := Item.Subject + Content;
+    end
+    else if Name = 'pubDate' then
+    begin
+      Item.Created := Item.Created + Content;
+    end
+    else if Name = 'managingEditor' then
+    begin
+      Item.Contact := Item.Contact + Content;
+    end
+    else if Name = 'generator' then
+    begin
+      Item.Generator := Item.Generator + Content;
+    end
+    else if Name = 'lastPubDate' then
+    begin
+      Item.LastModified := Item.LastModified + Content;
+    end
+    else if Name = 'language' then
+    begin
+      Item.Language := Item.Language + Content;
+      FXmlElement.Lang := Item.Language;
+    end
+    else if Name = 'copyright' then
+    begin
+      Item.Copyright := Item.Copyright + Content;
+    end
+    else if Name = 'guid' then
+    begin
+      Item.Id := Item.Id + Content;
+      Item.Uri := Item.Id;
     end;
   end;
 end;
